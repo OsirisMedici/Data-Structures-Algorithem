c32aae7754f4603f1cfb25d14d62b6a427d2de50
Abhishek <abhishekphd14@gmail.com>
2022-04-06 12:44:04 +0530

    Create some.cpp

---
+++ Assignment/some.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input;
+
+    cout << "Enter a thing in between A-z / a-z / 0-9 : " cin >> input;
+    if (input > A && input < Z)
+    {
+        cout << "YOu chose into A-z: ";
+    }
+}
