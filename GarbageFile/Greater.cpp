a154a78b4c63b00ee659d07c2b5bd637ee474eb1
Abhishek <abhishekphd14@gmail.com>
2022-04-06 12:43:52 +0530

    Create Greater.cpp

---
+++ GarbageFile/Greater.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "Enter a Nmber for A: ";
+    cin >> a;
+    cout << "Enter a Nmber for B: ";
+    cin >> b;
+
+    if (a > b)
+    {
+        cout << "A is greater Number" << endl;
+    }
+    else
+    {
+        cout << "B is greater Buddy" << endl;
+    }
+}
