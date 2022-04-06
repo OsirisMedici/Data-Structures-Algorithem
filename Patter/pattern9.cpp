9ebe828edc23be276a5648624a68932c88688d57
Abhishek <abhishekphd14@gmail.com>
2022-04-06 17:28:02 +0530

    Create pattern9.cpp

---
+++ Patter/pattern9.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a Limit: ";
+    int num;
+    cin >> num;
+
+    int row = 1;
+    while (row <= num)
+    {
+        int col = 1;
+        // int print = 0;
+        // int
+
+        while (col <= num)
+        {
+            char ch = 'A';
+            cout << ch << " ";
+
+            ch = ch + 1;
+
+            // col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
