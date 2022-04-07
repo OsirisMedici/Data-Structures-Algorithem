aeda298591e0744de4271b459fccfcb83bd3645e
Abhishek <abhishekphd14@gmail.com>
2022-04-07 23:17:10 +0530

    Create ReverseInteger.cpp

---
+++ LeetCode/ReverseInteger.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int ans = 0;
+    while (x != 0)
+    {
+        int digit = x % 10;
+        ans = (ans * 10) + digit;
+        x = x / 10;
+    }
+    cout << ans << endl;
+    return ans;
+}
