2d61d15c0d297c5e00db709fa037246ac43c9240
Abhishek <abhishekphd14@gmail.com>
2022-04-08 00:13:32 +0530

    Update NcR.cpp

--- Assignment/NcR.cpp
+++ Assignment/NcR.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 int Factorial(int n)
 {
-    for (int i = 0; i < n; i++)
+    int fact = 1;
+    for (int i = 0; i <= n; i++)
     {
-        int fact = 1;
+
         fact = fact * i;
-        return fact;
     }
+    return fact;
 }
 
 int nCr(int n, int r)
 {
-    int nume = Factorial * n;
+    int nume = Factorial(n);
 
-    int demon = (Factorial * r) * ((n - r) * Factorial);
+    int demon = Factorial(r) * Factorial(n - r);
 
     int answer = nume / demon;
     return answer;
@@ -24,9 +25,6 @@ int nCr(int n, int r)
 int main()
 {
     int n, r;
-    cin >> n >> r >> endl;
-}
-
-int main(int a, int b)
-{
+    cin >> n >> r;
+    cout << "Answer is " << nCr(n, r) << endl;
 }
