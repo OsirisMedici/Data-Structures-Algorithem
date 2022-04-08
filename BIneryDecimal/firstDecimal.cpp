d1bb6731de8aec8359b847a4cd70e89b3c401541
Abhishek <abhishekphd14@gmail.com>
2022-04-08 11:48:06 +0530

    Create firstDecimal.cpp

---
+++ BIneryDecimal/firstDecimal.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a Number You want binery form: ";
+    int deci;
+    cin >> deci;
+
+        int i = 0;
+    int ans = 0;
+
+    while (deci != 0)
+    {
+        int bit = deci & 1;
+        ans = (bit * pow(10, i)) + ans;
+
+        deci = deci >> 1;
+        i++;
+    }
+    cout << "Your Answer is: " << ans << endl;
+}
