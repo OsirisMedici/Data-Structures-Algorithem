abd9ffdb2eea0563e9472c8d7d21bd587c660632
Abhishek <abhishekphd14@gmail.com>
2022-04-10 13:18:20 +0530

    Create reverseArray.cpp

---
+++ 1-BinerySearch/reverseArray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int reverseArray(int arr[], int size)
+{
+    // cout << "size " << size << endl;
+    int end = size;
+
+    for (int i = 0; i <= size; i++)
+    {
+
+        swap(arr[i], arr[size - i]);
+    }
+    // cout << "size " << size << endl;
+
+    // return;
+}
+
+int main()
+{
+    int rArray[7] = {2, 5, 7, 11, 13, 17, 34};
+
+    reverseArray(rArray, 7);
+    printArray(rArray, 7);
+
+    return 0;
+}
