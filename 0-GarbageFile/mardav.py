f1e6b0e0038f17d29d941811fe690dbd0ab9c40a
Abhishek <abhishekphd14@gmail.com>
2022-04-18 22:05:07 +0530

    Create mardav.py

---
+++ 0-GarbageFile/mardav.py
@@ -0,0 +1,9 @@
+answer = input("give me the word you want to check: ")
+
+Lower = answer.lower()
+Upper = answer.upper()
+
+if (answer == Lower):
+    print("correct vroo")
+else:
+    print("Ja na lodu.. Upper case me chilla rahha hai")
