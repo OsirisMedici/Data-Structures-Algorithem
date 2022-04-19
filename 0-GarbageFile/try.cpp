ef491b964221ebbb89b19335f6eda0193689a9ed
Abhishek <abhishekphd14@gmail.com>
2022-04-19 20:24:30 +0530

    Create try.cpp

---
+++ 0-GarbageFile/try.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int printArr(int arr[], int size)
+{
+    cout << arr[size - 1];
+}
+
+int main()
+{
+    int arrrr[7] = {23, 12, 2, 4, 7, 54, 100};
+    int end = size - 1;
+
+    cout << "Haaan toh " << arrrr[end];
+
+    cout << "I mean yeahh: " << printArr;
+}
