0c0b06d9cc074f6a055ebdcc98cc1c017f2eceb4
Abhishek <abhishekphd14@gmail.com>
2022-04-30 21:45:41 +0530

    Create 10_natural.cpp

---
+++ 01-DSA CODE/Loops/10_natural.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int limit = 11;
+
+    for (int i = 1; i < limit; i++)
+    {
+        cout << i << " ";
+    }
+}
