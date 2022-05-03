abeadb434b31142a31a93ec68353bc9916efb831
Abhishek <abhishekphd14@gmail.com>
2022-05-03 17:18:36 +0530

    commmit

---
+++ 01-DSA CODE/Array/array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// int printArray(int arr[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+int main()
+{
+    int arr2[6] = {23, 34, 56, 78, 34, 56};
+
+    cout << arr2[5] << endl;
+
+    arr2.is_empty;
+
+    // cout << arr.length << endl;
+}
--- GarbageFile/mardav.py
+++
@@ -1,9 +0,0 @@
-answer = input("give me the word you want to check: ")
-
-Lower = answer.lower()
-Upper = answer.upper()
-
-if (answer == Lower):
-    print("correct vroo")
-else:
-    print("Ja na lodu.. Upper case me chilla rahha hai")
